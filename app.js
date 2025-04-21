b77d8fe754f81081436a3ac8e6f0b9881c2eb18b
sennaoz <161388670+sennaoz@users.noreply.github.com>
2025-04-21 23:18:46 +0300

    yorum satırı

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ document.write(24);
 //console.log ile çıktı verme
 console.log("hi");// consolde sitede gözüküyor
 console.clear();//console temizleme
+//debugger;
 
 //alert ile çıktı verme
 alert("uyarı verdi");//kullanıcıya uyarı veriyor window objesi en dış
 
+//console.log(window);//window objesi
+console.log(window); //sitede consoledakai metodları gösteriyor
+
+//yorum satırı
+// tek yorum satırı
+/*
+çoklu yorum satırı
+*/ 

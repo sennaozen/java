913fd4d448e76a28700665874faf1d0fb2805a28
sennaoz <161388670+sennaoz@users.noreply.github.com>
2025-04-21 21:40:54 +0300

    .write

---
+++ app.js
@@ -0,0 +1,5 @@
+//document write ilw çıktı verme
+document.write("merhabaa");
+document.write("<br>merhabaa");//br satır atlatma
+document.write("<h1>merhabaa</h1>");//h1 başlık
+document.write(24);
